a521bf9ee80c232d4f53b73ed96aca75750ec988
Azizaman <azizamanaaa97@gmail.com>
2025-02-20 22:05:33 +0530

    second commit

--- app/(main)/workspace/[id]/page.jsx
+++ app/(main)/workspace/[id]/page.jsx
@@ -1,18 +1,21 @@
-import Chatview from '@/components/custom/Chatview'
-import Codeview from '@/components/custom/Codeview'
-import React from 'react'
+"use client";
+import React from "react";
+import AppSideBar from "@/components/custom/AppSideBar";
+import Chatview from "@/components/custom/Chatview";
+import Codeview from "@/components/custom/Codeview";
 
 function Workspace() {
   return (
-    <div className='pl-8 pt-2 grid grid-cols-1 md:grid-cols-3 gap-4'>
-        <Chatview/>
-        <div className='col-span-2'>
-        <Codeview/>
-
+    <div className="flex">
+      <AppSideBar />
+      <div className="flex-1 pl-8 pt-2 grid grid-cols-1 md:grid-cols-3 gap-4">
+        <Chatview />
+        <div className="col-span-2">
+          <Codeview />
         </div>
-        
-        </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Workspace
+export default Workspace;
